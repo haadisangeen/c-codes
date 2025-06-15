b656d73d19ec4f7a1dc152ec47aebf6c1e95cf2f
haadi <fazansang@gmail.com>
2025-06-15 22:01:23 +0530

    displaying series

---
+++ displaying.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int n,i;
+
+    printf("Enter n\n");
+    scanf("%d",&n);
+
+    for (i=n; i>=1; i--);
+    {
+        printf("%d ",i);
+    }
+
+    return 0;
+}
