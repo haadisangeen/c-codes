8751e33d029c0759d74d46757b2ed4394f923552
haadi <fazansang@gmail.com>
2025-06-15 21:55:49 +0530

    turnary operator

---
+++ turnary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    double x,y;
+    printf("Give the two numbers.\n");
+    scanf("%lf,%lf",&x,&y);
+
+    (x>y)? printf("%lf",x):printf("%lf",y);
+
+    return 0;
+}
